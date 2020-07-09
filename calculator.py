063277fd15e37d1d414b74bfd46f642067e64f8b
Stephanie Yen <46392601+stephanieyen@users.noreply.github.com>
2020-07-09 09:26:18 -0400

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,5 @@
+import definitions
+# The line above will let you separate your concerns by defining functions your calculator might use in a separate file.
+
+print("Welcome to the Python Calculator\n\nWhat would you like to do?")
+choice = input()
